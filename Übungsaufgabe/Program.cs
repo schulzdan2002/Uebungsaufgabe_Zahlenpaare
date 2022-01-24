c2d146d2d876c3143cc691195ba01d6f3060a9a2
Dan Schulz <77929587+yoursteh@users.noreply.github.com>
2022-01-24 21:36:13 +0100

    Update Program.cs

--- Übungsaufgabe/Program.cs
+++ Übungsaufgabe/Program.cs
@@ -15,13 +15,13 @@ namespace Übungsaufgabe
                 b = 0;
             }
 
-            public zahlenpaar(int zahl1,int zahl2)      // Konstruktor dem Werte überbegebn können
+            public zahlenpaar(int zahl1,int zahl2)      // Konstruktor dem Werte überbegebn können als Integer
             {
                 a = zahl1;
                 b = zahl2;
             }
 
-            public zahlenpaar(string zahl)      // Konstruktor dem Werte überbegebn können im Stringformat
+            public zahlenpaar(string zahl)      // Konstruktor dem Werte überbegebn können als String
             {
                 int indexofk1, indexofk2, indexofkom, zahla,zahlb;
                 indexofk1 = zahl.IndexOf('(');
@@ -46,16 +46,16 @@ namespace Übungsaufgabe
                     zahlb = ((zahl[indexofkom + 1] - 48)*10)+zahl[indexofkom+2];
                 }
             }
-            public static zahlenpaar operator +(zahlenpaar a1,zahlenpaar b1) 
+            public static zahlenpaar operator +(zahlenpaar a1,zahlenpaar b1) // Additition von Zahlpaar 1 und Zahlenpaar 2
             {
-                zahlenpaar kekw2 = new zahlenpaar();
-                kekw2.a = a1.a + b1.a;
-                kekw2.b = a1.b + b1.b;
-                if (kekw2.a > 98)
-                    kekw2.a = 99;
-                if (kekw2.b > 98)
-                    kekw2.b = 99;
-                return (kekw2);
+                zahlenpaar z1 = new zahlenpaar();
+                z1.a = a1.a + b1.a;
+                z1.b = a1.b + b1.b;
+                if (z1.a > 98)
+                    z1.a = 99;
+                if (z1.b > 98)
+                    z1.b = 99;
+                return (z1);
             }
             public static bool operator <(zahlenpaar a1,zahlenpaar b1) 
             {
@@ -90,9 +90,9 @@ namespace Übungsaufgabe
                     return false;
             }
 
-            public static void ausgabe()
+            public void ausgabe()
             {
-                Console.WriteLine("{0},{1}");
+                Console.WriteLine("{0},{1}",a,b);
             }
 
 
@@ -100,22 +100,22 @@ namespace Übungsaufgabe
         }
         static void Main(string[] args)
         {
-            zahlenpaar kekw  = new zahlenpaar();
-            zahlenpaar kekw2 = new zahlenpaar();
-            Console.WriteLine("{0}",kekw+kekw2);
-            if (kekw<kekw2)
+            zahlenpaar z1  = new zahlenpaar(2,4);
+            zahlenpaar z2 = new zahlenpaar(0,5);
+            (z1 + z2).ausgabe();
+            if (z1<z2)
             {
-                Console.WriteLine("kekw");
+                Console.WriteLine("z2 ist größer");
             }
-            if (kekw > kekw2)
+            if (z1 > z2)
             {
-                Console.WriteLine("kekw");
+                Console.WriteLine("z2 ist kleiner");
             }
-            if (kekw == kekw2)
+            if (z1 == z2)
             {
-                Console.WriteLine("kekw");
+                Console.WriteLine("gleich");
             }
-            if (kekw != kekw2)
+            if (z1 != z2)
             {
                 Console.WriteLine("ungleich");
             }
